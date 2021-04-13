9c838183d0cc76690f98c3cd1c6d8c2f760dbfab
949361487sh <949361487@qq.com>
2021-04-13 19:57:54 +0800

    扫码

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -366,6 +366,7 @@ export default {
       }
     });
     this.getStockList();
+    this.scanCode();
   },
   data() {
     return {
@@ -406,6 +407,51 @@ export default {
     };
   },
   methods: {
+    scanCode() {
+      let code = "";
+      let lastTime, nextTime;
+      let lastCode, nextCode;
+      let that = this;
+      window.document.onkeypress = function (e) {
+        console.log("111111");
+        if (window.event) {
+          // IE
+          nextCode = e.keyCode;
+        } else if (e.which) {
+          // Netscape/Firefox/Opera
+          nextCode = e.which;
+        }
+        if (e.which === 13) {
+          debugger;
+          if (code.length < 3) return; // 手动输入的时间不会让code的长度大于2，所以这里只会对扫码枪有
+          console.log(code);
+          console.log("扫码结束");
+          that.inquiryGoods = code;
+          that.addSearch('扫码')
+          // that.distinguishCode(code) // 获取到扫码枪输入的内容，做别的操作
+          code = "";
+          lastCode = "";
+          lastTime = "";
+          return;
+        }
+        nextTime = new Date().getTime();
+        if (!lastTime && !lastCode) {
+          console.log("扫码开始。。。");
+          code += e.key;
+        }
+
+        if (lastCode && lastTime && nextTime - lastTime > 500) {
+          // 当扫码前有keypress事件时,防止首字缺失
+          console.log("防止首字缺失。。。");
+          code = e.key;
+        } else if (lastCode && lastTime) {
+          console.log("扫码中。。。");
+          code += e.key;
+        }
+        lastCode = nextCode;
+        lastTime = nextTime;
+      };
+    },
     addMemberSubmitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
@@ -431,17 +477,25 @@ export default {
         sex: "男生",
       };
     },
-    addSearch() {
+    addSearch(type) {
       searchStockAdd({
         code: this.inquiryGoods,
         commodityTitle: this.inquiryGoods,
       }).then((res) => {
         if (res.code == 0) {
+          
           res.data.map((item) => {
             item.number = 1;
           });
           this.stockData = res.data;
-          this.inquiryGoods = "";
+          
+          if(type === '扫码'){
+            this.addShopingList(res.data[0])
+          }else{
+            this.inquiryGoods = "";
+            }
+          
+          
         }
       });
     },
